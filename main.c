9d535991ca43a9d02485a8059581c257c32112d6
angelnamuli <angelnamuli@icloud.com>
2025-11-07 16:28:11 +0300

    Simple LED and button logic
    When the button is pressed, the LED turns on and when it is not pressed(closed), the LED turns off

--- main.c
+++ main.c
@@ -1,53 +1,35 @@
 /*
- * serial_comm_withLED.c
+ * sem_practice_LED.c
  *
- * Created: 10/16/2025 5:51:24 PM
+ * Created: 11/7/2025 3:52:51 PM
  * Author : ANGELREBECCA1
  */ 
 
 #include <avr/io.h>
-#define F_CPU 4000000UL
-
+#include <util/delay.h>
 
 int main(void)
 {
-	UBRRH = 0x00;
-	UBRRL = 0x19;
-	DDRC = 0xff;
-	unsigned char instruction[] = "Enter a number";
-	UCSRC |= (1<<4) | (1<<5); //odd parity
-	UCSRC |= (1<<1) | (1<<2); //8 bit size
-	UCSRB |= (1<<TXEN) | (1<<RXEN); // initializing TX and RX pins for serial communication
+	DDRD = 0XFF;
+	PORTD = 0x00;
+	PORTF = 0x00; //button
 	
-	//Transmission
-	for (int i=0;i <=14; i++){
-		while ((UCSRA & (1<<UDRE))==0);
-				UDR = instruction[i];
-	}
+	
+	
 	
 	
     /* Replace with your application code */
     while (1) 
-    { //receiving and LED logic
-		while ((UCSRA &(1<<RXC))==0);
-			char res = UDR;
-			
-			//controlling LED
-			if (res == '0'){
-				PORTC = (1<<PC0);
-			}
-			
-			if (res == '1'){
-				PORTC = (1<<PC1);	
-			}
-			
-			if (res == '2'){
-				PORTC = (1<<PC2);
-			}
-			
-			if (res == '3'){
-				PORTC = (1<<PC3);
-			}
+    {
+		if((PINF & 0b00000001)==0){
+			PORTD|= (1<<PD2);
+		}
+		else{
+			PORTD &= ~ (1<<PD2);
+		}
+		//turn the LED on and off
+		
+		
     }
 }
 
