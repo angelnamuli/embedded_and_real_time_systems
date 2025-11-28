47c788b15084c1303ec175b51c1c10335b457e7e
angelnamuli <angelnamuli@icloud.com>
2025-11-28 18:57:14 +0300

    added some missing code to main.c

--- main.c
+++ main.c
@@ -1,35 +1,92 @@
 /*
- * sem_practice_LED.c
+ * end_sem_keypad_7seg.c
  *
- * Created: 11/7/2025 3:52:51 PM
+ * Created: 11/27/2025 6:47:49 PM
  * Author : ANGELREBECCA1
  */ 
 
 #include <avr/io.h>
-#include <util/delay.h>
 
 int main(void)
 {
-	DDRD = 0XFF;
-	PORTD = 0x00;
-	PORTF = 0x00; //button
-	
-	
-	
-	
-	
+	DDRD = 0b00001111; //(PD0-PD3 are outputs on decoder)
+	DDRF = 0b11111000; //(PF3-PF6, outputs(rows), PF0-PF2, inputs(columns))
     /* Replace with your application code */
     while (1) 
     {
-		if((PINF & 0b00000001)==0){
-			PORTD|= (1<<PD2);
-		}
-		else{
-			PORTD &= ~ (1<<PD2);
-		}
-		//turn the LED on and off
 		
+		PORTF = 0b11110111; //Row A low(PF3=0), others high
+		//button 1
+		if ((PINF & 0b00000001)==0){
+			PORTD = 1;
+			while((PINF & 0b00000001)==0);
+		}
+		
+		//button 2
+		PORTF = 0b11110111; //Row A low(PF3=0), others high
+		if ((PINF & 0b00000010)==0){
+			PORTD = 2;
+			while((PINF & 0b00000010)==0);
+		}
+		
+		//button 3
+		PORTF = 0b11110111; //Row A low(PF3=0), others high
+		if ((PINF & 0b00000100)==0){
+			PORTD = 3;
+			while((PINF & 0b00000100)==0);
+		}
+		
+		//button 4
+		PORTF = 0b11101111; //row B(PF4=0), others high
+		if ((PINF &(1<<0))==0){
+			PORTD = 4;
+			while ((PINF &(1<<0))==0);
+		}
+		
+		//button 5
+		PORTF = 0b11101111; //row B(PF4=0), others high
+		if ((PINF &(1<<1))==0){
+			PORTD = 5;
+			while ((PINF &(1<<1))==0);
+		}
+		
+		//button 6
+		PORTF = 0b11101111; //row B(PF4=0), others high
+		if ((PINF &(1<<2))==0){
+			PORTD = 6;
+			while ((PINF &(1<<2))==0);
+		}
+		
+		//button 7
+		PORTF = 0b11011111; //row C(PF5=0), others high
+		if ((PINF &(1<<0))==0){
+			PORTD = 7;
+			while ((PINF &(1<<0))==0);
+		}
+		
+		//button 8
+		PORTF = 0b11011111; //row C(PF5=0), others high
+		if ((PINF &(1<<1))==0){
+			PORTD = 8;
+			while ((PINF &(1<<1))==0);
+		}
+		
+		//button 9
+		PORTF = 0b11011111; //row C(PF5=0), others high
+		if ((PINF &(1<<2))==0){
+			PORTD = 9;
+			while ((PINF &(1<<2))==0);
+		}
+		
+		//button 0
+		PORTF = 0b10111111; //row D low(PF6), others high
+		if ((PINF & (1<<1))==0){
+			PORTD = 0;
+			while ((PINF & (1<<1))==0);
+		}
+		
+		}
 		
     }
-}
+
 
